630fe9e72b9f0b4a864764dbc4778102205f816e
Kamran <Yousef.shaikhan@gmail.com>
2025-10-22 14:28:54 +0100

    refactor: added tasks array and displayTask() function for dynamic rendering

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+let tasks = []; //Array to holds all tasks
+
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 const addTaskButton = document.getElementById("add-task-btn");
 
 addTaskButton.addEventListener("click", function() {
     const taskText = taskInput.value.trim(); //Get the value from input field
-    
+
     if (taskText === "") return; // if input is empty, do nothing
 
-    const listItem = document.createElement("li"); //Create new list item
-    listItem.textContent = taskText; //set the text of the list item
-    taskList.appendChild(listItem); //Add the list item to the task list
+    tasks.push(taskText) //Add the new task to the end of the tasks array
+    
+    displayTasks(); //call function to create and display the task
 
     taskInput.value = ""; //Clear the input field
-
 });
+
+//Function to display all tasks
+function displayTasks() {
+    taskList.innerHTML = ""; //clear existing tasks
+
+    //Loop through tasks array and create list items
+    tasks.forEach(add => {
+        const listItem = document.createElement("li"); //Create new list item
+        listItem.textContent = add;  //set the text of the list item
+        taskList.appendChild(listItem); // Add the list item to the task list
+    });
+    
+}
